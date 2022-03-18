a5cb98bd6b5de0361caa3d2d5b1360030fe9c652
Carol <carol.tsai0224@gmail.com>
2022-03-18 16:22:44 -0400

    added showQuestion method

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,23 @@ var quiz = document.querySelector("#quiz");
 var timer = document.querySelector("#timer");
 
 var secondsLeft = 5;
+var questions = [
+   {
+      question: "Who is the author of the Harry Potter books?",
+      choices: ["C.S. Lewis", "J.K. Rowling", "J.R.R. Tolkein", "Rick Riordan"]
+   },
+   {
+      question: "Where does Harry go to school?",
+      choices: ["Princeton", "Oxford", "Hogwarts", "Yale"]
+   },
+   {
+      question: "What is the name of Harry's owl?",
+      choices: ["Bart", "Sally", "Sarah", "Hedwig"]
+   }
+];
+
+var questionPos = 0;
+
 
 function setTime() {
    // Sets interval in variable
@@ -22,11 +39,22 @@ function setTime() {
    }, 1000);
 }
 
+function showQuestion() {
+   var questionEl = document.getElementById("question-title");
+   questionEl.textContent = questions[questionPos].question;
+   for (i=0;i < questions[questionPos].choices.length; i++) {
+      var choice = document.getElementById("choice" + i);
+      choice.textContent = questions[questionPos].choices[i];
+      // TODO add event listener for each button
+   }
+}
+
 
 // When start button is clicked, timer starts and a question in presented
 startBtn.addEventListener("click", function () {
    firstPg.setAttribute("style", "display: none;");
    quiz.setAttribute("style", "display: block;");
+   showQuestion();
    timer.textContent = "Time: " + secondsLeft;
    setTime();
 })
