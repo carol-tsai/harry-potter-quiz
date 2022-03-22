d5f076c6d0ed1f8547bafe052272d2588028c650
Carol <carol.tsai0224@gmail.com>
2022-03-21 22:42:34 -0400

    when a question is answered incorrectly, time is subtracted from the countdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,33 @@ var secondsLeft = 30;
 var questions = [
    {
       question: "Who is the author of the Harry Potter books?",
-      choices: ["C.S. Lewis", "J.K. Rowling", "J.R.R. Tolkein", "Rick Riordan"]
+      choices: ["C.S. Lewis", "J.K. Rowling", "J.R.R. Tolkein", "Rick Riordan"],
+      correct: "J.K. Rowling"
    },
    {
       question: "Where does Harry go to school?",
-      choices: ["Princeton", "Oxford", "Hogwarts", "Eton"]
+      choices: ["Princeton", "Oxford", "Hogwarts", "Eton"],
+      correct: "Hogwarts"
    },
    {
       question: "What is the name of Harry's owl?",
-      choices: ["Bart", "Sally", "Sarah", "Hedwig"]
+      choices: ["Bart", "Sally", "Sarah", "Hedwig"],
+      correct: "Hedwig"
+   },
+   {
+      question: "Which of these is NOT a teacher at Hogwarts",
+      choices: ["Professor Snape", "Professor Wormtail", "Professor Umbridge", "Hagrid"],
+      correct: "Professor Wormtail"
+   },
+   {
+      question: "What is the name of Harry's owl?",
+      choices: ["Bart", "Sally", "Sarah", "Hedwig"],
+      correct: "Hedwig"
+   },
+   {
+      question: "Which Harry Potter book features a flying car?",
+      choices: ["Chamber of Secrets", "Goblet of Fire", "Sorcerer's Stone", "Half Blood Prince"],
+      correct: "Chamber of Secrets"
    }
 ];
 
@@ -33,7 +51,7 @@ function setTime() {
          // Stops execution of action at set interval
          clearInterval(timerInterval);
          // Calls function to create and append image
-         sendMessage();
+         sendMessage(); //Todo change to show end page
       }
 
    }, 1000);
@@ -47,24 +65,31 @@ function showQuestion() {
       choice.textContent = questions[questionPos].choices[i];
       // TODO add event listener for each button
    }
+   if (questionPos>questions.length) {
+      // Todo take people to ending page
+   }
+}
+
+function nextQuestion(event) {
+   var answer = event.target;
+   var correct = questions[questionPos].correct;
+   if (answer.textContent != correct) {
+      secondsLeft -= 5;
+   }
    questionPos++;
-}
-
-function nextQuestion() {
-
+   showQuestion();
 }
 
 
-// When start button is clicked, timer starts and a question in presented
+// When start button is clicked, timer starts and a question in presented. When an answer is clicked, another question is displayed
 startBtn.addEventListener("click", function () {
    firstPg.setAttribute("style", "display: none;");
    quiz.setAttribute("style", "display: block;");
    showQuestion();
-   console.log(questionPos);
    timer.textContent = "Time: " + secondsLeft;
    setTime();
    for (i = 0; i < questions[questionPos].choices.length; i++) {
       var choice = document.getElementById("choice" + i);
-      choice.addEventListener("click", showQuestion);
+      choice.addEventListener("click", nextQuestion);
    }
 })
